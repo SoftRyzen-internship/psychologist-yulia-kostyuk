115af7af3d7cff15d05383bea5015e9e2d7c1be0
Svitlana Zhovanik <lanasvetcat@gmail.com>
2024-03-14 14:36:10 +0200

    init husky

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,13 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { apiVersion, dataset, projectId } from './sanity/env';
+import { schema } from './sanity/schema';
 
 export default defineConfig({
   basePath: '/admin',
@@ -20,6 +20,6 @@ export default defineConfig({
     structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
-})
+});
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,5 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [],
-}
+};
