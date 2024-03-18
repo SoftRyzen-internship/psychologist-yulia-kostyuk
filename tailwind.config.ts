62f3d93dea950c523470f12e664dc71f41077855
Svitlana Zhovanik <lanasvetcat@gmail.com>
2024-03-18 13:07:31 +0200

    added styles and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,8 +32,24 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)'],
+        tenor: ['var(--font-tenor)'],
+      },
+      fontSize: {
+        lightLarge: ['32px', '1.25'],
+        large: ['40px', '1.2'],
+        extraLarge: ['56px', '1.07'],
+      },
       colors: {
+        mainBcg: '#E5E1D5',
+        footerBcg: '#D6D0C5',
+        cardBcg: '#FAF5ED',
         accent: '#395734',
+        text: '#292929',
+        error: '#CB3D3D',
+        hover: '#2C4228',
+        pressed: '#21331F',
       },
     },
   },
