f8927475d544e2ccd7f743279b931daa78e0a575
SerhiiRud <carmagsales@gmail.com>
2024-03-19 16:49:39 +0200

    json, styling

--- src/components/ui/BurgerMenu/BurgerMenu.tsx
+++ src/components/ui/BurgerMenu/BurgerMenu.tsx
@@ -1,31 +1,35 @@
 import Image from 'next/image';
 import { Logo } from '../Logo';
 import { IBurgerMenuProps } from './types';
+import { menuItems } from '../../../data/header.json';
 
 export const BurgerMenu = ({ isOpen, onClose }: IBurgerMenuProps) => {
   return (
     <>
       <div className="bg-backdrop and fixed bottom-0 right-0 z-10 h-full w-full backdrop-blur-xl">
-        <div className="mx-auto h-[600px] w-[360px] bg-mainBcg px-5 py-[22px]">
-          <div className="flex">
+        <div className="mx-auto h-[640px] w-[360px] bg-mainBcg pb-12 pl-5 pr-[25px] pt-[22px]">
+          <div className="mb-[62px] flex justify-between">
             <Logo path="header" onClick={onClose} />
             <button aria-label="menu button close" onClick={onClose}>
               <Image
                 src="/icons/close-icon.svg"
                 alt="menu open button"
                 priority={true}
-                width={20}
-                height={20}
+                width={14}
+                height={14}
               />
             </button>
           </div>
-          <nav>
+          <nav className="mb-[340px] ml-3">
             <ul>
-              <li>Про мене</li>
-              <li>Пщслуги</li>
-              <li>Контакти</li>
+              {menuItems.map(item => (
+                <li key={item} className="mb-6 last:mb-0">
+                  {item}
+                </li>
+              ))}
             </ul>
           </nav>
+          <div>Socials</div>
         </div>
       </div>
     </>
