6bae1d467fd800c64812e54860a1f63a034f3cc5
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-03-19 11:45:21 +0200

    add button component/ add navLinks.json

---
+++ src/components/ui/TestUI/Button/Button.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import classNames from 'classnames';
+import { ButtonProps } from './types';
+
+export const Button: React.FC<ButtonProps> = ({
+  tag: Tag = 'a',
+  accent = true,
+  children,
+  href,
+  buttonType,
+  onClick,
+  disabled = false,
+  className,
+}) => {
+  const btnStyles = classNames(
+    'cursor-pointer block text-center w-full text-white',
+    {
+      'py-4 px-12 max-w-80 bg-accent xl:max-w-[198px]': 
+      accent === true,
+      'hover:bg-hover focus:bg-hover active:bg-pressed': 
+      accent === true,
+      'py-3 px-6 max-w-80 bg-transparent border-solid border-accent xl:max-w-[187px]':
+      accent === false,
+      'hover:bg-hsla(0, 0%, 16%, 0.04) focus:bg-hsla(0, 0%, 16%, 0.04) active:bg-hsla(0, 0%, 16%, 0.08)':
+      accent === false,
+    },
+    className,
+  );
+
+  return (
+    <Tag
+      onClick={(e: React.MouseEvent) => {
+        if (onClick) onClick();
+
+        if (buttonType === 'submit' && onClick) onClick();
+      }}
+      href={href}
+      type={buttonType}
+      className={className ? `${btnStyles} ${className}` : btnStyles}
+      disabled={disabled}
+    >
+      {children}
+    </Tag>
+  );
+};
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Image from 'next/image';
 import Meeting from '@/../public/icons/meeting.svg';
+import common from '@/data/common.json';
+import { Button } from '@/components/ui/TestUI/Button';
 
 export const Hero = () => {
   return (
@@ -13,6 +17,9 @@ export const Hero = () => {
           height={500}
           alt="Котик с ножичком))"
         />
+        <Button tag="a" accent={true} className="font-montserrat font-semibold">
+        {common.buttonsText.v1}
+          </Button>
         <Meeting />
       </div>
     </section>
