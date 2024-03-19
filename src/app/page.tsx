44a42e16926ae87f70467f5add63072b2dbce293
Tokio <innka-pinnka@ukr.net>
2024-03-19 16:04:20 +0200

    create request for services sanity schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/sections/Hero';
+import Services from '@/components/common/Services/Services';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
