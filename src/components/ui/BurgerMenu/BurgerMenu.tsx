e979fa0c12c427146d9138fd6cf7f5d90f854c65
SerhiiRud <carmagsales@gmail.com>
2024-03-20 14:49:44 +0200

    paddings

--- src/components/ui/BurgerMenu/BurgerMenu.tsx
+++ src/components/ui/BurgerMenu/BurgerMenu.tsx
@@ -20,8 +20,8 @@ export default function BurgerMenu({ onClose }: IBurgerMenuProps) {
   return (
     <>
       <div className="bg-backdrop and fixed bottom-0 right-0 z-10 h-full w-full overscroll-none backdrop-blur-xl xl:hidden">
-        <div className="mx-auto flex h-lvh w-full flex-col bg-mainBcg">
-          <div className="mb-[52px] flex justify-between px-5 py-[22px] md:pl-8">
+        <div className="mx-auto flex h-lvh w-full flex-col  bg-mainBcg">
+          <div className=" flex justify-between px-5 py-[22px] md:px-8 md:py-[26px]">
             <Logo path="header" onClick={onClose} />
             <button
               aria-label="menu button close"
@@ -31,11 +31,13 @@ export default function BurgerMenu({ onClose }: IBurgerMenuProps) {
               <CloseIcon width={14} height={14} />
             </button>
           </div>
-          <nav className="mb-auto  ml-3 self-start md:ml-0">
-            <Navbar variant="mobile-menu" />
-          </nav>
-          <div className="self-center md:self-start">
-            <Socials />
+          <div className=" flex h-full flex-col justify-between px-8 py-10 md:py-[60px]">
+            <nav className="mb-auto self-start">
+              <Navbar variant="mobile-menu" />
+            </nav>
+            <div className="self-center md:self-start">
+              <Socials />
+            </div>
           </div>
         </div>
       </div>
