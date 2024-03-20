1778a5cd502b18c227a8c8892c335b28d4eb4915
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-03-20 14:09:35 +0200

    commit

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -15,8 +15,7 @@ export const Button: React.FC<ButtonProps> = ({
   const btnStyles = classnames(
     'cursor-pointer font-montserrat inline-block text-center',
     {
-      'py-4 px-12 max-w-80 bg-accent text-white transition':
-        accent === true,
+      'py-4 px-12 max-w-80 bg-accent text-white transition': accent === true,
       'hover:bg-hover focus:bg-hover active:bg-pressed': accent === true,
       'py-3 px-6 max-w-80 bg-transparent text-accent transition border border-solid border-accent xl:max-w-[187px]':
         accent === false,
