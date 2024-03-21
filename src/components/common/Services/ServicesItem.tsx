9bd0d764aa625f9e08cd89a845495e02bf28628c
Tokio <innka-pinnka@ukr.net>
2024-03-21 10:12:14 +0200

    Create Services compomnent

--- src/components/common/Services/ServicesItem.tsx
+++ src/components/common/Services/ServicesItem.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Cards } from './types';
-
-interface ServiceCardProps {
-  card: Cards;
-}
+import { ServiceCardProps } from './types';
 
 const ServiceItem: React.FC<ServiceCardProps> = ({ card }) => {
   return (
-    <li className="relative flex flex-col font-semibold  before:absolute before:left-[-35px] before:top-[-2px] before:h-[24px] before:w-[24px] before:content-arrow">
+    <li className="relative ml-[45px] flex flex-col font-semibold before:absolute  before:left-[-43px] before:top-0 before:h-6 before:w-6 before:content-arrow md:ml-11 xl:w-[592px]">
       <p className="text">{card.subtitle}</p>
       {card.description && (
         <p className="text font-normal">{card.description}</p>
