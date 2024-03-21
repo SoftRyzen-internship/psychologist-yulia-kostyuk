c1b4a999cb2ba18fbe2ef76224d71c65b5a4b4b3
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-03-21 18:21:24 +0200

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { Navbar } from '@/components/ui/Navbar';
 import { Hero } from '@/sections/Hero';
 import { Button } from '@/components/ui/Button';
 import { Socials } from '@/components/ui/Socials';
@@ -8,6 +8,7 @@ import { Logo } from '@/components/ui/Logo';
 import common from '@/data/common.json';
 
 import Services from '@/components/common/Services/Services';
+import { Conditions } from '@/components/common/Conditions';
 
 export default function Home() {
   const onLogoClick = () => {
@@ -16,8 +17,17 @@ export default function Home() {
 
   return (
     <>
+      <div className="container relative flex items-center">
+        <Logo
+          path="header"
+          onClick={onLogoClick}
+          className="mr-[90px] xl:ml-[102px]"
+        />
+        <Navbar variant="header" />
+      </div>
       <Hero />
       <Services />
+      <Conditions />
       <Button tag="a" accent={true} className="font-semibold">
         {common.buttonsText.v1}
       </Button>
@@ -27,9 +37,8 @@ export default function Home() {
       <Button tag="a" accent={false} className="font-normal">
         {common.buttonsText.v1}
       </Button>
-
-      <Logo path="footer" onClick={onLogoClick} stylesLogo="ml-7" />
-      <Socials stylesSocials="mt-5" />
+      <Logo path="footer" onClick={onLogoClick} className="ml-7" />
+      <Socials className="mt-5" />
     </>
   );
 }
