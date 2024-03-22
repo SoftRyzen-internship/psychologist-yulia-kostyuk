4a47f4265d9f3b9568a9e601b481b0aef68b2f23
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-03-22 18:52:22 +0200

    icon bug

--- src/components/common/MyValues/MyValues.tsx
+++ src/components/common/MyValues/MyValues.tsx
@@ -7,14 +7,15 @@ export const MyValues: React.FC = () => {
   return (
     <div className="container">
       <div>
-        <h2 className='section-title text-accent mb-10 md:mb-12 xl:mb-[60px] font-tenor'>{myValues.title}</h2>
+        <h2 className="section-title mb-10 font-tenor text-accent md:mb-12 xl:mb-[60px]">
+          {myValues.title}
+        </h2>
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-3 ">
           {myValues.values.map((value: Value) => (
             <ValueItem key={value.id} value={value} />
           ))}
         </div>
       </div>
-
     </div>
   );
 };
--- src/components/common/MyValues/ValueItem.tsx
+++ src/components/common/MyValues/ValueItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import myValues from '@/data/myValues.json';
 import { Value } from './types';
 import Lock from '@/../public/icons/lock.svg';
 import Meeting from '@/../public/icons/meeting.svg';
@@ -10,21 +11,19 @@ interface ValueItemProps {
 
 export const ValueItem: React.FC<ValueItemProps> = ({ value }) => {
   return (
-    <div
-      key={value.id}
-      className="p-6 md:p-8 xl:p-10 flex flex-col bg-cardBcg md:flex-row xl:flex-col justify-center items-center"
-    >
-      <div className=''>
-        {value.name === "lock" && <Lock className='h-[20px] w-[20px] md:w-[64px] md:h-[64px]' />}
-        {value.name === "meeting" && <Meeting className='h-[20px] w-[20px] md:w-[64px] md:h-[64px]' />}
-        {value.name === "flower" && <Flower className='h-[20px] w-[20px] md:w-[64px] md:h-[64px]' />}
+
+    <div className="p-6 md:p-8 xl:p-10 flex flex-col bg-cardBcg md:flex-row xl:flex-col justify-center items-center">
+      <div className=' mb-6 md:mb-0 '>
+        {value.name === "lock" && <Lock className="h-12 w-12 md:h-16 md:w-16" width={48} height={48} />}
+        {value.name === "meeting" && <Meeting className="h-12 w-12 md:h-16 md:w-16" width={48} height={48} />}
+        {value.name === "flower" && <Flower className="h-12 w-12 md:h-16 md:w-16" width={48} height={48} />}
       </div>
-
-
       <div className='flex flex-col justify-center items-center text-center'>
         <h3 className="mb-3 text-xl font-normal text-accent font-tenor tracking-[0.5px] md:mb-4 xl:mb-6">{value.subTitle}</h3>
         <p>{value.description}</p>
       </div>
     </div>
-  );
-};
+
+
+  )
+}
