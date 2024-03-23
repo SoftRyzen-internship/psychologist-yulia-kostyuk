9d38cda89e01cc79aa7a6b8f56857a97fc0462cb
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-03-23 11:02:08 +0200

    add files

--- src/components/common/MyValues/ValueItem.tsx
+++ src/components/common/MyValues/ValueItem.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import myValues from '@/data/myValues.json';
 import { Value } from './types';
 import Lock from '@/../public/icons/lock.svg';
 import Meeting from '@/../public/icons/meeting.svg';
@@ -11,33 +10,32 @@ interface ValueItemProps {
 
 export const ValueItem: React.FC<ValueItemProps> = ({ value }) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-cardBcg p-6 md:flex-row md:p-8 xl:flex-col xl:p-10">
-      <div className=" mb-6 md:mb-0 ">
+    <div className="flex flex-col items-center justify-center bg-cardBcg p-6 md:flex-row md:p-8 xl:flex-col xl:p-10 mdOnly:gap-8">
+      <div className=" mb-6 md:mb-0 xl:mb-10">
         {value.name === 'lock' && (
-          <Lock className="h-12 w-12 md:h-16 md:w-16" width={48} height={48} />
+          <Lock className="h-12 w-12 xl:h-16 xl:w-16" width={48} height={48} />
         )}
         {value.name === 'meeting' && (
           <Meeting
-            className="h-12 w-12 md:h-16 md:w-16"
+            className="h-12 w-12 xl:h-16 xl:w-16"
             width={48}
             height={48}
           />
         )}
         {value.name === 'flower' && (
           <Flower
-            className="h-12 w-12 md:h-16 md:w-16"
+            className="h-12 w-12 xl:h-16 xl:w-16"
             width={48}
             height={48}
           />
         )}
       </div>
-      <div className="flex flex-col items-center justify-center text-center">
+      <div className="flex flex-col items-center justify-center text-center mdOnly:items-start mdOnly:text-start">
         <h3 className="mb-3 font-tenor text-xl font-normal tracking-[0.5px] text-accent md:mb-4 xl:mb-6">
           {value.subTitle}
         </h3>
-        <p>{value.description}</p>
+        <p className="text">{value.description}</p>
       </div>
     </div>
   );
 };
-
--- src/sections/Hero/index.ts
+++ src/sections/Hero/index.ts
@@ -1,1 +1,3 @@
 export * from './Hero';
+export * from '../Values/Values';
+export * from '../Services/Services';
