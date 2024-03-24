d2fe1cb250395aa7f6929162cf254851f7cf5f90
Tokio <innka-pinnka@ukr.net>
2024-03-24 22:32:18 +0200

    Fix MyValues Section view

--- src/sections/Values/Values.tsx
+++ src/sections/Values/Values.tsx
@@ -8,7 +8,7 @@ export const Values: React.FC = () => {
     <section className="mp-20 w-full pb-10 md:pb-[60px] md:pt-[120px] xl:pb-20 xl:pt-40">
       <div className="container">
         <div>
-          <h2 className="section-title mb-10 font-tenor text-accent md:mb-12 xl:mb-[60px]">
+          <h2 className="section-title mb-10  font-tenor text-accent md:mb-12 xl:mb-[60px] ">
             {myValues.title}
           </h2>
           <div className="grid grid-cols-1 gap-8 xl:grid-cols-3 ">
